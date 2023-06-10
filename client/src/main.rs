10a4369497eb5140da64af65c70bf486e79ba73e
Rybeusz100 <janzych@protonmail.com>
2023-06-10 17:49:53 +0200

    refactor

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,4 @@
+use crate::utils::{convert_to_bool_array, parse_board_coordinates};
 use async_std::{net::TcpStream, task};
 #[cfg(feature = "automatic")]
 use rand::Rng;
@@ -10,6 +11,8 @@ use std::{
     net::{Ipv4Addr, UdpSocket},
     time::Duration,
 };
+
+mod utils;
 
 async fn run_client() -> io::Result<()> {
     #[cfg(feature = "automatic")]
@@ -111,44 +114,4 @@ fn main() -> io::Result<()> {
         run_client().await?;
         Ok(())
     })
-}
-
-fn convert_to_bool_array(input: [[i32; 10]; 10]) -> [[bool; 10]; 10] {
-    let mut bool_array = [[false; 10]; 10];
-
-    for i in 0..10 {
-        for j in 0..10 {
-            bool_array[i][j] = input[i][j] != 0;
-        }
-    }
-
-    bool_array
-}
-
-fn parse_board_coordinates(input: &str) -> Result<(u8, u8), String> {
-    if input.len() != 2 {
-        return Err("Invalid input length".to_string());
-    }
-
-    let x_char = input.chars().next().ok_or("Invalid input")?;
-    let y_char = input.chars().nth(1).ok_or("Invalid input")?;
-
-    let x_offset = b'A';
-    let y_offset = b'0';
-
-    let x_val = x_char as u8;
-    let y_val = y_char as u8;
-
-    if x_val < b'A' || y_val < b'0' {
-        return Err("Invalid board coordinates".to_string());
-    }
-
-    let x = x_val - x_offset;
-    let y = y_val - y_offset;
-
-    if x > 9 || y > 9 {
-        return Err("Invalid board coordinates".to_string());
-    }
-
-    Ok((x, y))
 }
